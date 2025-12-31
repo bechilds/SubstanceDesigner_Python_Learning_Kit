b032a1e68434b4888a0e350238fa439fd69f806a
yuanzijian_lgCopilot <bechildsmax@gmail.com>
2025-12-31 19:15:35 +0800

    更新

--- Bilibili_HuangJuanLr/SubstanceDesignerPart2/mylib/window.py
+++ Bilibili_HuangJuanLr/SubstanceDesignerPart2/mylib/window.py
@@ -1,4 +1,11 @@
+#这个文件是教程作者拆分出来的一个子文件，主要用于定义一个窗口类window。
+
 from PySide2 import QtWidgets, QtCore, QtGui, QtUiTools
+# 导入 PySide2 模块 用于界面操作,pyside2 是qt的python接口,QtWidgets用于创建和操作GUI元素，QtCore包含核心非GUI功能，QtGui用于图形相关功能，QtUiTools用于加载.ui文件
+
+
+#教程作者发现拆除多个文件后，在substance designer中调试时，需要重启SD才能更新插件，很不方便。所以后续将这部分功能合并到一个文件中，方便调试。
+#但是教程后续没有再拆解除这个文件了。所以这个文件实际上没有被使用到。
 
 #构建window类，继承自QtWidgets.QDialog
 class window(QtWidgets.QDialog):
@@ -23,6 +30,12 @@ class window(QtWidgets.QDialog):
 
         #self.window ，！！！AI对这句有疑问，认为冗余。后面再测试理解！！！
         self.window = QtWidgets.QDialog(parent=parent) #创建一个对话框窗口，parent是父窗口
+
+        # 设置窗口标题,进行测试
+        layout = QtWidgets.QVBoxLayout() #创建一个垂直布局管理器，用于管理窗口中的控件布局
+        self.testLineEdit = QtWidgets.QLineEdit("Test") #创建一个单行文本编辑框
+        layout.addWidget(self.testLineEdit) #把文本编辑框添加到布局管理器中
+        self.window.setLayout(layout) #把布局管理器设置为窗口的布局
 
 
     def show(self):
